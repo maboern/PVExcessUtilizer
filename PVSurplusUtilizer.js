87b273ecae721380ee9bb37ca3006fd889f0725f
Markus Berner <Markus.Berner@gmx.ch>
2025-03-07 23:41:47 +0100

    r Extract PVSystem from PVSurplusUtilizer

--- PVSurplusUtilizer.js
+++ PVSurplusUtilizer.js
@@ -1,48 +1,17 @@
-// Debug
-var debug = 1; /*debug ausgabe ein oder aus 1/0 */
-
 // Static Script Parameters
 var SCRIPT_UPDATE_INTERVAL_SEC = 30;
 var SCRIPT_MAX_VALUE_AGE = 60 * 1000;
 var SCRIPT_LOAD_HISTERESYS_WATTS = 250;
+var SCRIPT_DEBUG = true;
 var scriptRunTimeSecs = 0;
 var SCRIPT_ID = 'javascript.0';
-
-// Static PV System Parameters
-var PV_PEAK_POWER_WATTS = 16400;
-var PV_GRID_FEEDIN_MAX_WATTS = 13200;
-var PV_TOTAL_NR_PV_PANELS = INV15_NR_PV_PANELS + INV10_NR_PV_PANELS;
-var PV_PANEL_POWER_WATTS = 440;
-var PV_MANAGER_CONNECTED = 'sma-em.0.info.connection';
-var PV_GRID_FEEDIN_ID = 'sma-em.0.3014001810.psurplus';
-
 
 var SCRIPT_READY_OBJ = SCRIPT_ID + ".info.ready";
 createState(SCRIPT_READY_OBJ, false, {read: true, write: false, name: "ready", type: "boolean", def: false});
 var SCRIPT_RUNTIME_SECS_OBJ = SCRIPT_ID + ".info.runTimeSecs";
 createState(SCRIPT_RUNTIME_SECS_OBJ, 0, {read: true, write: false, name: ".info.runTimeSecs", type: "number", unit: "s", def: 0});
-
-var SCRIPT_FEEDIN_MAX_WATTS_OBJ = SCRIPT_ID + ".info.feedin_max_watts";
-createState(SCRIPT_FEEDIN_MAX_WATTS_OBJ, 0, {read: true, write: false, name: "gridFeedInMaxWatts", type: "number", unit: "W", def: PV_GRID_FEEDIN_MAX_WATTS});
-var SCRIPT_PV_CUR_WATTS_OBJ = SCRIPT_ID + ".info.pv_cur_watts";
-createState(SCRIPT_PV_CUR_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionCurrentPower", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_POT_WATTS_OBJ = SCRIPT_ID + ".info.pv_potential_watts";
-createState(SCRIPT_PV_POT_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionPotentialPower", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_FEEDIN_WATTS_OBJ = SCRIPT_ID + ".info.pv_feedin_watts";
-createState(SCRIPT_PV_FEEDIN_WATTS_OBJ, 0, {read: true, write: false, name: "pvFeedInPower", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_FEEDIN_LIMIT_DEVIATION_WATTS_OBJ = SCRIPT_ID + ".info.pv_feedin_limit_deviation_watts";
-createState(SCRIPT_PV_FEEDIN_LIMIT_DEVIATION_WATTS_OBJ, 0, {read: true, write: false, name: "pvFeedInLimitDeviation", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_SURPLUS_WATTS_OBJ = SCRIPT_ID + ".info.pv_surplus_watts";
-createState(SCRIPT_PV_SURPLUS_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionSurplusPower", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_FORECASTED_SURPLUS_WATTS_OBJ = SCRIPT_ID + ".info.pv_surplus_forecast_watts";
-createState(SCRIPT_PV_FORECASTED_SURPLUS_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionSurplusForecastPower", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_AVAILABLE_EXCESS_WATTS_OBJ = SCRIPT_ID + ".info.pv_available_excess_watts";
-createState(SCRIPT_PV_AVAILABLE_EXCESS_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionAvailableExcessPower", type: "number", unit: "W", def: 0});
-var SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ = SCRIPT_ID + ".info.pv_self_consumption_watts";
-createState(SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ, 0, {read: true, write: false, name: "pvSelfConsumptionPower", type: "number", unit: "W", def: 0});
 var SCRIPT_CONTROLLED_LOADS_WATTS_OBJ = SCRIPT_ID + ".info.controlled_loads_watts";
 createState(SCRIPT_CONTROLLED_LOADS_WATTS_OBJ, 0, {read: true, write: false, name: "controlledLoadPower", type: "number", unit: "W", def: 0});
-
 
 var SCRIPT_LOAD_CELLAR_HEATER_RUNTIME_OBJ = SCRIPT_ID + ".info.load.cellarHeater.runtimeSecs";
 createState(SCRIPT_LOAD_CELLAR_HEATER_RUNTIME_OBJ, 0, {read: true, write: false, name: "loadCellarHeaterRuntimeSecs", type: "number", unit: "s", def: 0});
@@ -62,23 +31,6 @@ createState(SCRIPT_LOAD_BOILER_TOTAL_ENERGY_OBJ, 0, {read: true, write: false, n
 var SCRIPT_LOAD_BOILER_DAY_ENERGY_OBJ = SCRIPT_ID + ".info.load.boiler.todayEnergy";
 createState(SCRIPT_LOAD_BOILER_DAY_ENERGY_OBJ, 0, {read: true, write: false, name: "loadBoilerTodayEnergy", type: "number", unit: "Ws", def: 0});
 
-// Sunny TriPower X-15 15kW Inverter
-var INV15_MAX_POWER_WATTS = 15000;
-var INV15_NR_PV_PANELS = 35;
-var INV15_MODBUS_ID = 'modbus.0';
-var INV15_CONNECTED_OBJ = INV15_MODBUS_ID + '.info.connection';
-var INV15_CUR_POWER_WATTS_OBJ = INV15_MODBUS_ID + '.inputRegisters.60776_Measurement_GridMs_TotW'
-
-// Sunny TriPower SmartEnergy 10kW Hybrid Inverter
-var INV10_MAX_POWER_WATTS = 10000;
-var INV10_NR_PV_PANELS = 25;
-var INV10_MODBUS_ID = 'modbus.1';
-var INV10_CONNECTED_OBJ = INV10_MODBUS_ID + '.info.connection';
-var INV10_CUR_POWER_WATTS_OBJ = INV10_MODBUS_ID + '.inputRegisters.60776_Measurement_GridMs_TotW';
-
-// PV Forecast from api.forecast.solar
-var PV_FORECAST_POWER_NOW_OBJ = 'pvforecast.0.summary.power.now';
-
 // Shelly SmartPlug as with Space Heater
 var LOAD_SHELLY_CELLAR_HEATER_EXPECTED_POWER_WATTS = 2000;
 var LOAD_SHELLY_CELLAR_HEATER_CONNECTED = 'shelly.0.info.connection';
@@ -94,8 +46,8 @@ var LOAD_BOILER_SWITCH = 'modbus.2.coils.0_DO-00';
 var load_boiler_runtime_secs = 0;
 var load_boiler_day_energy = 0;
 
-let pv_system = new PVSystem('Turnerweg');
-console.log(pv_system.getDetails());
+let pv = new PVSystem('Turnerweg');
+console.log(pv.getDetails());
 
 function getRecentStateVal(obj) {
     var state = getState(obj);
@@ -112,9 +64,7 @@ function updateRunTime() {
 }
 
 function checkReady() {
-    if(getState(INV10_CONNECTED_OBJ).val 
-    && getState(INV15_CONNECTED_OBJ).val
-    && getState(PV_MANAGER_CONNECTED).val) {
+    if(pv.checkReady()) {
         setState(SCRIPT_READY_OBJ, true, true);
         updateRunTime();
         return true;
@@ -125,47 +75,6 @@ function checkReady() {
     }
 }
 
-function updatePVProduction() {
-    var inv10_power = getState(INV10_CUR_POWER_WATTS_OBJ).val;
-    var inv15_power = getState(INV15_CUR_POWER_WATTS_OBJ).val;
-
-    var feedin_power = getState(PV_GRID_FEEDIN_ID).val;
-    setState(SCRIPT_PV_FEEDIN_WATTS_OBJ, feedin_power, true);
-    var pv_feedin_limit_deviation = feedin_power - PV_GRID_FEEDIN_MAX_WATTS;
-    setState(SCRIPT_PV_FEEDIN_LIMIT_DEVIATION_WATTS_OBJ, pv_feedin_limit_deviation, true);
-
-    var pv_power = inv10_power + inv15_power;
-    setState(SCRIPT_PV_CUR_WATTS_OBJ, pv_power, true);
-
-    /* The 15kW Inverter should produce more than the 10kW inverter proportional to the number
-       of PV Panels on the string. */
-    var inv15_potential_pv_production = inv10_power * INV15_NR_PV_PANELS / INV10_NR_PV_PANELS;
-    var potential_pv_production = inv10_power + inv15_potential_pv_production;
-    setState(SCRIPT_PV_POT_WATTS_OBJ, potential_pv_production, true);
-
-    var self_consumption = pv_power - feedin_power;
-    setState(SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ, self_consumption, true);
-
-    var pv_surplus = 0;
-    if(potential_pv_production > pv_power && potential_pv_production > PV_GRID_FEEDIN_MAX_WATTS) {
-        pv_surplus = potential_pv_production - pv_power;
-    } 
-    setState(SCRIPT_PV_SURPLUS_WATTS_OBJ, pv_surplus, true);
-
-    var pv_forecast = getState(PV_FORECAST_POWER_NOW_OBJ).val;
-    var pv_forecasted_surplus = 0;
-    var unused_excess_power = 0;
-    if(pv_forecast > pv_power && pv_power >= PV_GRID_FEEDIN_MAX_WATTS) {
-        pv_forecasted_surplus = pv_forecast - pv_power;
-        unused_excess_power = pv_forecasted_surplus + pv_feedin_limit_deviation;
-    } 
-    setState(SCRIPT_PV_FORECASTED_SURPLUS_WATTS_OBJ, pv_forecasted_surplus, true);
-    var available_excess_power = unused_excess_power > 0 ? unused_excess_power : 0;
-    setState(SCRIPT_PV_AVAILABLE_EXCESS_WATTS_OBJ, available_excess_power, true);
-
-    // TODO: Calculate real available excess power including controlled load power (Object oriented)
-    return unused_excess_power;
-}
 
 function updateCellarHeaterRunTime(water_heater_on) {
     if(water_heater_on) {
@@ -266,10 +175,9 @@ function controlLoads(available_excess_power) {
     var load_boiler_power = 0;
     if(boiler_connected) {
         boiler_on = getState(LOAD_BOILER_SWITCH).val;
-        var self_consumption = getState(SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ).val;
 
         if(boiler_on) {
-            var non_load_consumption = self_consumption - load_cellar_heater_power - LOAD_BOILER_EXPECTED_POWER_WATTS;
+            var non_load_consumption = pv.getSelfConsumption() - load_cellar_heater_power - LOAD_BOILER_EXPECTED_POWER_WATTS;
             if(non_load_consumption > 0) {
                 load_boiler_power = LOAD_BOILER_EXPECTED_POWER_WATTS;
             } else {
@@ -319,7 +227,7 @@ function controlLoads(available_excess_power) {
 function processing() {
     if(!checkReady()) { return; }
 
-    var available_excess_power = updatePVProduction();
+    var available_excess_power = pv.updatePVProduction();
     controlLoads(available_excess_power);
 }
 
--- PVSystem.js
+++ PVSystem.js
@@ -1,5 +1,5 @@
 class PVSystem {
-    constructor(name, maker, engine) {
+    constructor(name) {
         this.name = name;
         this.SCRIPT_ID = 'javascript.0';
         this.updateIntervalMs = 1000;
@@ -10,6 +10,7 @@ class PVSystem {
         this.INV15_MODBUS_ID = 'modbus.0';
         this.INV15_CONNECTED_OBJ = this.INV15_MODBUS_ID + '.info.connection';
         this.INV15_CUR_POWER_WATTS_OBJ = this.INV15_MODBUS_ID + '.inputRegisters.60776_Measurement_GridMs_TotW'
+        this.inv15_power = 0;
 
         // Sunny TriPower SmartEnergy 10kW Hybrid Inverter
         this.INV10_MAX_POWER_WATTS = 10000;
@@ -17,9 +18,10 @@ class PVSystem {
         this.INV10_MODBUS_ID = 'modbus.1';
         this.INV10_CONNECTED_OBJ = this.INV10_MODBUS_ID + '.info.connection';
         this.INV10_CUR_POWER_WATTS_OBJ = this.INV10_MODBUS_ID + '.inputRegisters.60776_Measurement_GridMs_TotW';
+        this.inv10_power = 0;
 
         // PV Forecast from api.forecast.solar
-        var PV_FORECAST_POWER_NOW_OBJ = 'pvforecast.0.summary.power.now';
+        this.PV_FORECAST_POWER_NOW_OBJ = 'pvforecast.0.summary.power.now';
 
         // Static PV System Parameters
         this.PV_PEAK_POWER_WATTS = 16400;
@@ -27,10 +29,14 @@ class PVSystem {
         this.PV_TOTAL_NR_PV_PANELS = this.INV15_NR_PV_PANELS + this.INV10_NR_PV_PANELS;
         this.PV_PANEL_POWER_WATTS = 440;
         this.PV_MANAGER_CONNECTED = 'sma-em.0.info.connection';
-        this.PV_GRID_FEEDIN_ID = 'sma-em.0.3014001810.psurplus';
+        this.PV_GRID_FEEDIN_OBJ = 'sma-em.0.3014001810.psurplus';
+
+        this.feedin_power = 0;
+        this.pv_feedin_limit_deviation = 0;
+        this.self_consumption = 0;
 
         this.SCRIPT_FEEDIN_MAX_WATTS_OBJ = this.SCRIPT_ID + ".info.feedin_max_watts";
-        createState(this.SCRIPT_FEEDIN_MAX_WATTS_OBJ, 0, {read: true, write: false, name: "gridFeedInMaxWatts", type: "number", unit: "W", def: PV_GRID_FEEDIN_MAX_WATTS});
+        createState(this.SCRIPT_FEEDIN_MAX_WATTS_OBJ, 0, {read: true, write: false, name: "gridFeedInMaxWatts", type: "number", unit: "W", def: this.PV_GRID_FEEDIN_MAX_WATTS});
         this.SCRIPT_PV_CUR_WATTS_OBJ = this.SCRIPT_ID + ".info.pv_cur_watts";
         createState(this.SCRIPT_PV_CUR_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionCurrentPower", type: "number", unit: "W", def: 0});
         this.SCRIPT_PV_POT_WATTS_OBJ = this.SCRIPT_ID + ".info.pv_potential_watts";
@@ -47,8 +53,6 @@ class PVSystem {
         createState(this.SCRIPT_PV_AVAILABLE_EXCESS_WATTS_OBJ, 0, {read: true, write: false, name: "pvProductionAvailableExcessPower", type: "number", unit: "W", def: 0});
         this.SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ = this.SCRIPT_ID + ".info.pv_self_consumption_watts";
         createState(this.SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ, 0, {read: true, write: false, name: "pvSelfConsumptionPower", type: "number", unit: "W", def: 0});
-        this.SCRIPT_CONTROLLED_LOADS_WATTS_OBJ = this.SCRIPT_ID + ".info.controlled_loads_watts";
-        createState(this.SCRIPT_CONTROLLED_LOADS_WATTS_OBJ, 0, {read: true, write: false, name: "controlledLoadPower", type: "number", unit: "W", def: 0});
 
         console.log(`Initialized PV System ${this.name} (${this.PV_PEAK_POWER_WATTS} Wp).`)
     }
@@ -57,6 +61,62 @@ class PVSystem {
         return (`The name of the system is ${this.name}.`)
     }
 
+    getSelfConsumption() {
+        return this.self_consumption;
+    }
+
+    checkReady() {
+        if(getState(this.INV10_CONNECTED_OBJ).val 
+        && getState(this.INV15_CONNECTED_OBJ).val
+        && getState(this.PV_MANAGER_CONNECTED).val) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    updatePVProduction() {
+        this.inv10_power = getState(this.INV10_CUR_POWER_WATTS_OBJ).val;
+        this.inv15_power = getState(this.INV15_CUR_POWER_WATTS_OBJ).val;
+
+        this.feedin_power = getState(this.PV_GRID_FEEDIN_OBJ).val;
+        setState(this.SCRIPT_PV_FEEDIN_WATTS_OBJ, this.feedin_power, true);
+        this.pv_feedin_limit_deviation = this.feedin_power - this.PV_GRID_FEEDIN_MAX_WATTS;
+        setState(this.SCRIPT_PV_FEEDIN_LIMIT_DEVIATION_WATTS_OBJ, this.pv_feedin_limit_deviation, true);
+
+        var pv_power = this.inv10_power + this.inv15_power;
+        setState(this.SCRIPT_PV_CUR_WATTS_OBJ, pv_power, true);
+
+        /* The 15kW Inverter should produce more than the 10kW inverter proportional to the number
+        of PV Panels on the string. */
+        var inv15_potential_pv_production = this.inv10_power * this.INV15_NR_PV_PANELS / this.INV10_NR_PV_PANELS;
+        var potential_pv_production = this.inv10_power + inv15_potential_pv_production;
+        setState(this.SCRIPT_PV_POT_WATTS_OBJ, potential_pv_production, true);
+
+        this.self_consumption = pv_power - this.feedin_power;
+        setState(this.SCRIPT_PV_SELF_CONSUMPTION_WATTS_OBJ, this.self_consumption, true);
+
+        var pv_surplus = 0;
+        if(potential_pv_production > pv_power && potential_pv_production > this.PV_GRID_FEEDIN_MAX_WATTS) {
+            pv_surplus = potential_pv_production - pv_power;
+        } 
+        setState(this.SCRIPT_PV_SURPLUS_WATTS_OBJ, pv_surplus, true);
+
+        var pv_forecast = getState(this.PV_FORECAST_POWER_NOW_OBJ).val;
+        var pv_forecasted_surplus = 0;
+        var unused_excess_power = 0;
+        if(pv_forecast > pv_power && pv_power >= this.PV_GRID_FEEDIN_MAX_WATTS) {
+            pv_forecasted_surplus = pv_forecast - pv_power;
+            unused_excess_power = pv_forecasted_surplus + this.pv_feedin_limit_deviation;
+        } 
+        setState(this.SCRIPT_PV_FORECASTED_SURPLUS_WATTS_OBJ, pv_forecasted_surplus, true);
+        var available_excess_power = unused_excess_power > 0 ? unused_excess_power : 0;
+        setState(this.SCRIPT_PV_AVAILABLE_EXCESS_WATTS_OBJ, available_excess_power, true);
+
+        // TODO: Calculate real available excess power including controlled load power (Object oriented)
+        return unused_excess_power;
+    }
+    
     process() {
         
     }
